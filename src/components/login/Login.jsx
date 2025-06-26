899bd1de41896be3c668864c413eec01ddd8cf30
vicky70 <salmankhan08a@gmail.com>
2025-06-26 20:16:12 +0530

    added error message

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -40,7 +40,7 @@ const Login = ({setIsLoggedIn}) => {
                 alert(data.error || 'Registration Failed')
             }
         }catch(error){
-            alert('No Network connection. please check your connection')
+            alert('No Network connection. please check your connection', error)
         }
     };
 
